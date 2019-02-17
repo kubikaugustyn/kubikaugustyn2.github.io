b695383a94ca460f864948444547c706f015957b
raugustyn <raugustyn@post.cz>
2019-02-17 17:06:52 +0100

    Initial commit

---
+++ PythonLessons/01_VypisNaObrazovku.py
@@ -0,0 +1,17 @@
+#  -*- coding: utf-8 -*-
+__author__ = "raugustyn@post.cz"
+
+# Tohle je začátek výpisu
+print "Kubíček"
+print "============================" # Tohle je oddělovač
+znamky = [1, 1, 2, 1, 1, 8, 2]
+pocetZnamek = len(znamky) + 6
+print pocetZnamek, znamky, znamky[4]
+#print "pocetZnamek", pocetZnamek, "!!!"
+print "============================"
+print "Konec výpisu" # A tady je konec pocetZnamek
+
+radek = ""
+for sloupec in range(50):
+    radek += "%d*56=%d\n" % (sloupec, sloupec*56)
+print radek
