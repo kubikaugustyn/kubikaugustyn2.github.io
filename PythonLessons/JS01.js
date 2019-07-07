f84078ee40a1f274e7f04103053b533ace4d72b2
raugustyn <raugustyn@post.cz>
2019-07-07 08:59:12 +0200

    Working update...

--- PythonLessons/JS01.js
+++ PythonLessons/JS01.js
@@ -1,50 +1,21 @@
 console.log("JS01.js ...");
 
-function onClickProc1() {
+function ClickProc1() {
     console.log("Opening Seznam.cz...");
     openLinkInNewTab('https://www.seznam.cz/');
 }
 
-function onClickProc2() {
+function ClickProc2() {
     console.log("Opening YouTube...");
     openLinkInNewTab('https://youtube.com/');
 }
 
-function onClickProc3() {
+function ClickProc3() {
     console.log("Opening YouTube Channel Tech Ideas...");
     openLinkInNewTab('https://youtube.com/channel/UCNtV2t2MX3qGkBSD_uRtCGg');
 }
 
-function onClickProc4() {
+function ClickProc4() {
     console.log("Closing All Opened Tabs...");
     CloseTab('https://youtube.com/channel/UCNtV2t2MX3qGkBSD_uRtCGg'&'https://youtube.com/');
-}
-
-function onClickProc5() {
-    console.log("Reloading Page...");
-    page=("reload");
-    console.log("aaa");
-    console.log("aaa");
-    console.log("aaa");
-    console.log("aaa");
-    console.log("aaa");
-    console.log("aaa");
-    console.log("aaa");
-    console.log("aaa");
-    console.log("aaa");
-    console.log("aaa");
-    console.log("aaa");
-    console.log("aaa");
-    console.log("aaa");
-    console.log("aaa");
-    console.log("aaa");
-    console.log("aaa");
-    console.log("aaa");
-    console.log("aaa");
-    console.log("aaa");
-    console.log("aaa");
-    console.log("aaa");
-    console.log("aaa");
-    console.log("aaa");
-    console.log("aaa");
 }
