05b1731e0cbf96a5ffd814ba2afe5860694533b0
raugustyn <Marika56gi>
2019-08-09 09:29:47 +0200

    Working update...

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function Minus() {
     console.log("Klávesa stisknuta...");
     var msg = ""
     if (document.getElementById("input01").value == MOJE_HESLO) {
-        msg = "<a href='https://kubikaugustyn.github.io/'>Kubíkovo web</a>"
+        msg = "<a href='PythonLessons/PoHeslu.html'>Kubíkovo web</a>"
     }
     document.getElementById("neco").innerHTML = msg
 }
