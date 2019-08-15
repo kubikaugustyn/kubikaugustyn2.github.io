2b5aa2af68fef8437452bed33b52281234544ac4
raugustyn <Marika56gi>
2019-08-15 18:43:10 +0200

    Working update...

---
+++ PythonLessons/Tlačítka3.js
@@ -0,0 +1,33 @@
+function ButtonProc1(sender) {
+    console.log(sender);
+    sender.disabled=true;
+}
+
+function ButtonProc2(sender) {
+    console.log(sender);
+    sender.disabled=true;
+}
+
+function ButtonProc3(sender) {
+    console.log(sender);
+    sender.disabled=true;
+}
+
+function ButtonProc4(sender) {
+    console.log(sender);
+    sender.disabled=true;
+}
+
+function ButtonProc5(sender) {
+    console.log(sender);
+    sender.disabled=true;
+}
+
+function ButtonProc6() {
+    console.log("6");
+}
+
+ function openLinkInNewTab(url) {
+        var win = window.open(url, '_blank');
+        win.focus();
+    }
