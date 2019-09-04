2d84cf9392df8befe169fb8496ab440a65bf02ce
raugustyn <Marika56gi>
2019-09-04 15:54:34 +0200

    Working update...

--- index.js
+++ index.js
@@ -1,22 +1,10 @@
-var MOJE_HESLO = "bbb"
+var pageVariants = {
+    "bbb" : "Vítejte Paní učitelko (Pane učiteli)!!!<br><a href='PythonLessons/Html/UcitelPoHeslu.html'>Kubíkův web</a>",
+    "ccc": "Vítej Žáku!!!<br><a href='PythonLessons/Html/ZakPoHeslu.html'>Kubíkův web</a>",
+    "dddd": "<a href='PythonLessons/Html/PoHeslu.html'>Kubíkův web</a>"
+}
 
-var MOJE_HESLOb = "ccc"
-
-var MOJE_HESLOc = "dddd"
-
-function Minus() {
+function onKeyPress() {
     console.log("Klávesa stisknuta...");
-    var msg = ""
-    if (document.getElementById("input01").value == MOJE_HESLO) {
-        msg = "Vítejte Paní učitelko (Pane učiteli)!!!<br><a href='PythonLessons/Html/UcitelPoHeslu.html'>Kubíkův web</a>"
-    }
-
-      if (document.getElementById("input01").value == MOJE_HESLOb) {
-        msg = "Vítej Žáku!!!<br><a href='PythonLessons/Html/ZakPoHeslu.html'>Kubíkův web</a>"
-    }
-
-       if (document.getElementById("input01").value == MOJE_HESLOc) {
-        msg = "<a href='PythonLessons/Html/PoHeslu.html'>Kubíkův web</a>"
-    }
-    document.getElementById("neco").innerHTML = msg
+    document.getElementById("neco").innerHTML = pageVariants[document.getElementById("input01").value]
 }
