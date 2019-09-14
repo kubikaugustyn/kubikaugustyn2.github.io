2b191341c062c11fc517606d7db7f13168fbdd21
raugustyn <Marika56gi>
2019-09-14 12:03:05 +0200

    Working update...

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var pageVariants = {
-    "aaa" : "<br><a href='https://decko.ceskatelevize.cz/hry/kutej-spunte'><img alt='Kutej špunte' src='Kutej_špunte.png'></a>",
+    "aaa" : "<br><a href='https://decko.ceskatelevize.cz/hry/kutej-spunte'><img alt='Kutej špunte' src='Kutej_spunte.png'></a>",
     "bbb" : "Vítejte Paní učitelko (Pane učiteli)!!!<br><a href='PythonLessons/Html/UcitelPoHeslu.html'>Kubíkův web</a>",
     "ccc": "Vítej Žáku!!!<br><a href='PythonLessons/Html/ZakPoHeslu.html'>Kubíkův web</a>",
     "dddd": "<a href='PythonLessons/Html/PoHeslu.html'>Kubíkův web</a>",
