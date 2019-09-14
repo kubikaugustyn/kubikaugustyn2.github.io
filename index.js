3e84528afce28fe107ce3b5c2a5fa969efc50236
raugustyn <Marika56gi>
2019-09-14 11:38:45 +0200

    Working update...

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var pageVariants = {
+    "aaa" : "<a href='https://decko.ceskatelevize.cz/hry'>Déčko hry</a>",
     "bbb" : "Vítejte Paní učitelko (Pane učiteli)!!!<br><a href='PythonLessons/Html/UcitelPoHeslu.html'>Kubíkův web</a>",
     "ccc": "Vítej Žáku!!!<br><a href='PythonLessons/Html/ZakPoHeslu.html'>Kubíkův web</a>",
     "dddd": "<a href='PythonLessons/Html/PoHeslu.html'>Kubíkův web</a>",
