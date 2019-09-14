63b2ef1d4a57e6df09c08a2b023d3d7885d92189
raugustyn <Marika56gi>
2019-09-14 11:56:49 +0200

    Working update...

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var pageVariants = {
-    "aaa" : "<a href='https://decko.ceskatelevize.cz/hry'>Déčko hry</a>",
+    "aaa" : "<br><a href='https://decko.ceskatelevize.cz/hry/kutej-spunte'><img alt='Kutej špunte' src='Kutej_špunte.png'></a>",
     "bbb" : "Vítejte Paní učitelko (Pane učiteli)!!!<br><a href='PythonLessons/Html/UcitelPoHeslu.html'>Kubíkův web</a>",
     "ccc": "Vítej Žáku!!!<br><a href='PythonLessons/Html/ZakPoHeslu.html'>Kubíkův web</a>",
     "dddd": "<a href='PythonLessons/Html/PoHeslu.html'>Kubíkův web</a>",
-    "eee" : "Ahoj Ondro!!!<br><a href='PythonLessons/Ondra/Ondra1.html'"
+    "eee" : "Ahoj Ondro!!!<br><a href='PythonLessons/Ondra/Ondra1.html'>Ondráškův web</a>"
 }
 
 function onKeyPress() {
