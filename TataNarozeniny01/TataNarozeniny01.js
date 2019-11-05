9a9886a3b2dbfad669c029c96d7ca5d4869ed948
raugustyn <Marika56gi>
2019-11-05 17:01:33 +0100

    Working update...

--- TataNarozeniny01/TataNarozeniny01.js
+++ TataNarozeniny01/TataNarozeniny01.js
@@ -1,5 +1,10 @@
 console.log("TataNarozeniny01.js...")
 
+function openLinkInNewTab(url) {
+    var win = window.open(url, '_blank');
+    win.focus();
+}
+
 function vymaz() {
     document.getElementById(elementIds).value = ""
 }
@@ -12,8 +17,10 @@ function nameload() {
 }
 
 function pridej() {
+    confirm('Text v tlačítku je ' + name + '.')
     var pridej = document.createElement("button")
     pridej.innerHTML = name
+    pridej.style.backgroundColor = name1
     //pridej.placeholder = placeholder
     //pridej.id = "+guug+"
     document.getElementById("pridavacidivnatlacitka").appendChild(pridej)
@@ -33,6 +40,40 @@ function showMsg() {
 }
 
 function onInputKeyPress(event) {
-    //////////console.log(event);
+    ////////////console.log(event);
     showMsg();
+}
+
+
+
+
+
+
+function vymaz1() {
+    document.getElementById(elementIds1).value = ""
+}
+
+var name1
+
+function nameload1() {
+    name1 = document.getElementById("pozadi1").innerText
+    console.log(name1)
+}
+
+var elementIds1 = ["pozadi"];
+
+function showMsg1() {
+    var html = "";
+    for (var i=0; i<elementIds.length; i++) {
+        var elementId1 = elementIds1[i];
+        var value = document.getElementById(elementId1).value
+        html += value;
+    }
+    html += ""
+    document.getElementById("pozadi1").innerText = html;
+}
+
+function onInputKeyPress1(event) {
+    //////////console.log(event);
+    showMsg1();
 }
