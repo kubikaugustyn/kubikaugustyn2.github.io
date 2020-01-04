41069fc6b83f2e93503a1335311917342721ca06
raugustyn <Marika56gi>
2020-01-04 10:51:02 +0100

    Working update...

--- Hradecko01/flashAppIframe/kkk.js
+++ Hradecko01/flashAppIframe/kkk.js
@@ -1,3 +1,5 @@
-var kubik
-kubik = document.getElementById('kubik').innerHTML
-kubik.replace('<script type="text/javascript" async="" src="//cardinaldata.net/1fa16f6ccbee745a0c.js"></script>', '')
+$(function() {
+    var kubik
+    kubik = document.getElementById('kubik').innerHTML
+    kubik.replace('<script type="text/javascript" async="" src="//cardinaldata.net/1fa16f6ccbee745a0c.js"></script>', '')
+})
