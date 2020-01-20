eb09aa93b59353d21cb9337b9899c162af00b8e4
kubikaugustyn <47718385+kubikaugustyn@users.noreply.github.com>
2020-01-20 15:28:24 +0100

    Update keys.js

--- NodeApps/Prihlasovani01/config/keys.js
+++ NodeApps/Prihlasovani01/config/keys.js
@@ -1,6 +1,4 @@
-dbPassword1 = 'mongodb+srv://YOUR_USERNAME_HERE:'+ encodeURIComponent('YOUR_PASSWORD_HERE') + '@CLUSTER_NAME_HERE.mongodb.net/test?retryWrites=true';
-
-dbPassword = 'mongodb+srv://JakubAugustyn:kubik2009@cluster0-2oq3t.mongodb.net/test?retryWrites=true&w=majority'
+dbPassword = 'mongodb+srv://YOUR_USERNAME_HERE:'+ encodeURIComponent('YOUR_PASSWORD_HERE') + '@CLUSTER_NAME_HERE.mongodb.net/test?retryWrites=true';
 
 module.exports = {
     mongoURI: dbPassword
