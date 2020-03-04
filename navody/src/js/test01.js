49957cb903566861aa9c0bce88231883a9563c39
raugustyn <Marika56gi>
2020-03-04 17:42:30 +0100

    Working update...

--- navody/src/js/test01.js
+++ navody/src/js/test01.js
@@ -1,46 +1,51 @@
 console.log('test01.js...')
 
 var test01 = {
-    "0": {
+    0: {
         "kostky": "",
         "pocet" : "",
         "img": "vysledek"
     },
-    "1": {
+    1: {
         "kostky": "modre/IMG_8899",
         "pocet" : "4",
-        "img": "fg"
+        "img": "IMG_8604"
     },
-    "2": {
+    2: {
         "kostky": "modre/IMG_8900",
         "pocet" : "4",
-        "img": "fg"
+        "img": "IMG_8608"
     },
-    "3": {
+    3: {
         "kostky": "modre/IMG_8925",
         "pocet" : "4",
-        "img": "fg"
+        "img": "IMG_8609"
     },
-    "4": {
+    4: {
         "kostky": "modre/IMG_8923",
         "pocet" : "4",
-        "img": "fg"
+        "img": "IMG_8610"
     },
-    "5": {
+    5: {
         "kostky": "modre/IMG_8903",
         "pocet" : "4",
-        "img": "fg"
+        "img": "IMG_8611"
     },
-    "6": {
-        "kostky": "oranzove/IMG_8960",
-        "pocet" : "48",
-        "img": "fg"
+    6: {
+        "kostky": "oranzove/IMG_8960;bile/IMG_8902",
+        "pocet" : "48;95",
+        "img": "IMG_8612"
     },
-    "7": {
-        "kostky": "oranzove/IMG_8950",
-        "pocet" : "3",
-        "img": "fg"
+    7: {
+        "kostky": "oranzove/IMG_8950;bile/IMG_8909",
+        "pocet" : "3;4",
+        "img": "IMG_8613"
+    },
+    8: {
+        "kostky": "oranzove/IMG_8950;bile/IMG_8909;bile/IMG_890",
+        "pocet" : "49;72;61",
+        "img": "IMG_8613"
     }
 }
 
-console.log(test01)
+//console.log(test01)
--- navody/src/js/userGuideMaker.js
+++ navody/src/js/userGuideMaker.js
@@ -1,6 +1,6 @@
 console.log('userGuideMaker.js...')
 
-var test01copy = test01
+//var test01copy = test01
 
 var test01Length
 
@@ -13,7 +13,7 @@ for (var i = 0; i<1000; i++) {
     else {
         if (test01LengthOk === 0) {
             test01Length = i
-            console.log("test01 length is ", test01Length)
+            //console.log("test01 length is ", test01Length)
         }
         test01LengthOk++
     }
@@ -22,19 +22,78 @@ for (var i = 0; i<1000; i++) {
 function loadUserGuide() {
     for (i = 0; i < test01Length; i++) {
         //console.log(test01[i])
-        if (test01[i]["kostky"] === "") {
-        } else {
-            //console.log(test01[i]["kostky"])
-            test01[i]["kostky"] = "../src/img/kostky/" + test01[i]["kostky"] + ".JPG"
+        if (test01[i]["kostky"] === "" && test01[i]["pocet"] === "") {
             test01[i]["img"] = "../src/img/postup/" + test01[i]["img"] + ".JPG"
-            //console.log(test01[i]["kostky"])
             document.getElementById('strany').innerHTML += "" +
                 "<img height='100px' width='100px' src='" +
-                test01[i]["kostky"] +
-                "' alt='" + test01[i]["kostky"] +
+                test01[i]["img"] +
+                "' alt='" + test01[i]["img"] +
                 "'>" +
-                test01[i]["pocet"] +
-                "X" +
+                "<br>" +
+                "<hr>"
+        }
+        else {
+            var kostkyLenght = test01[i]["kostky"].split(";").length
+            function loadImgName() {
+                for (var index = 0; index < kostkyLenght; index++) {
+                    if (index === kostkyLenght-1){console.error("Ending...")}
+                    console.error("Next...")
+                    var test01Copy
+                    if (index === 0) {test01Copy = ""}
+                    console.log(index, test01Copy)
+                    console.log(test01[i]["kostky"].split(";")[index])
+                    if (index === 0) {
+                        test01Copy += "../src/img/kostky/" + test01[i]["kostky"].split(";")[index] + ".JPG"
+                    }
+                    else {
+                        test01Copy += ";../src/img/kostky/" + test01[i]["kostky"].split(";")[index] + ".JPG"
+                    }
+                    console.log(index, test01Copy)
+                    test01[i]["kostky"] = test01Copy
+                    console.warn(index, test01[i]["kostky"])
+                }
+            }
+            loadImgName()
+            test01[i]["img"] = "../src/img/postup/" + test01[i]["img"] + ".JPG"
+            var items = test01[i]["kostky"].split(";")
+
+            if(items.length === test01[i]["pocet"].split(";").length) {
+                console.log("OK")
+            } else {
+                console.error("ERR", test01[i])
+            }
+
+            console.log("items.length, items:", items.length, items)
+            if (items.length === 0){
+                document.getElementById('strany').innerHTML = "ERR"
+                console.error("ERR", test01[i])
+            }
+            else if (items.length === 1){
+                document.getElementById('strany').innerHTML += "" +
+                    "<img height='50px' width='50px' src='" +
+                    test01[i]["kostky"] +
+                    "' alt='" + test01[i]["kostky"] +
+                    "'>" +
+                    "<br>" +
+                    test01[i]["pocet"] +
+                    "X" +
+                    "<br>"
+            }
+            else if (items.length > 1){
+                for (var ind = 0; ind<items.length;ind++) {
+                    document.getElementById('strany').innerHTML += "" +
+                        "<img height='50px' width='50px' src='" +
+                        test01[i]["kostky"].split(";")[ind] +
+                        "' alt='" + test01[i]["kostky"].split(";")[ind] +
+                        "'>" +
+                        "<br>" +
+                        test01[i]["pocet"].split(";")[i] +
+                        "X" +
+                        "<br>"
+                }
+            }
+
+            document.getElementById('strany').innerHTML += "" +
                 "<img height='100px' width='100px' src='" +
                 test01[i]["img"] +
                 "' alt='" + test01[i]["img"] +
