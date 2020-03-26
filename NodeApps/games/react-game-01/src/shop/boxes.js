dd29c029c29bca0f3fac132cf45f4d9083da9c99
kubikaugustyn <kubik.augustyn@post.cz>
2020-03-26 11:47:59 +0100

    Working update...

--- NodeApps/games/react-game-01/src/shop/boxes.js
+++ NodeApps/games/react-game-01/src/shop/boxes.js
@@ -2,13 +2,14 @@ import React from 'react';
 
 export default function boxes() {
   const imgUrl = "https://kubikaugustyn.github.io/NodeApps/games/react-game-01/src/img/";
+  const imgWidth = "10%";
 
   return (
       <div>
-        <img alt="Box" src={imgUrl + "Box.png"} />
-        <img alt="bigBox" src={imgUrl + "bigBox.png"} />
-        <img alt="megaBox" src={imgUrl + "megaBox.png"} />
-        <img alt="magicBox" src={imgUrl + "magicBox.png"} />
+        <img width={imgWidth} alt="Box" src={imgUrl + "Box.png"} />
+        <img width={imgWidth} alt="bigBox" src={imgUrl + "bigBox.png"} />
+        <img width={imgWidth} alt="megaBox" src={imgUrl + "megaBox.png"} />
+        <img width={imgWidth} alt="magicBox" src={imgUrl + "magicBox.png"} />
       </div>
   );
 }
