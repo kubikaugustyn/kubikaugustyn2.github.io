67f6d70be1ac324ed5306d6f734b03b22fe667be
kubikaugustyn <kubik.augustyn@post.cz>
2020-05-03 21:05:00 +0200

    Working update...

--- NodeApps/Prihlasovani01/config/keys.js
+++
@@ -1,9 +0,0 @@
-/*
-    author Kubík Augustýn, kubik.augustyn@post.cz
-*/
-
-const dbPassword = 'mongodb+srv://JakubAugustyn:kubik2009@cluster0-2oq3t.mongodb.net/test?retryWrites=true&w=majority'
-
-module.exports = {
-    mongoURI: dbPassword
-};
--- NodeApps/Prihlasovani01/src/Game1.js
+++
@@ -1,3 +0,0 @@
-function playGame() {
-    document.getElementById('game1').innerHTML += '<br>Level:' + game1DataLevel[1] + '<br>Stage:' + game1DataStage[1]
-}
