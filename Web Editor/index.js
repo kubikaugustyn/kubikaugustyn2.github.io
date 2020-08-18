8949347529ee3883c43a8def845813ced757cd34
kubikaugustyn <kubik.augustyn@post.cz>
2020-08-18 16:03:36 +0200

    Working update at 18. 7. 2020...

--- Web Editor/index.js
+++ Web Editor/index.js
@@ -1,9 +1,19 @@
 //var LeftDivHeight
 
+var Templates = [
+    '<!DOCTYPE html>\n<html lang="en">\n<head>\n    <meta charset="UTF-8>',
+    "</head>\n<body>",
+    "</body>\n</html>"
+]
+
 function DownloadAsFile() {
+    /*var url = document.getElementById('body').value +
+        '&' +
+        document.getElementById('head').value
+    url = SearchAndReplace(SearchAndReplace(SearchAndReplace("" + url, "\n", ""), "\r", ""), "\t", "")
     // read text from URL location
     var request = new XMLHttpRequest();
-    request.open('GET', "./Wiew_Edited_Web_Page.html" + url, true);
+    request.open('GET', "./Wiew_Edited_Web_Page.html?" + url, true);
     //request.open('GET', "https://www.youtube.com/embed/5zlOHpoARRA", true);
     request.send(null);
     request.onreadystatechange = function () {
@@ -22,11 +32,22 @@ function DownloadAsFile() {
             hiddenElement.download = 'my_edited_html.html';
             hiddenElement.click();
         }
-    }
+    }*/
+
+    var textToSave = Templates[0] + document.getElementById('body').value + Templates[1] + document.getElementById('head').value + Templates[3]
+    textToSave = "data:attachment/text," + textToSave;
+    //console.log(textToSave)
+    var hiddenElement = document.createElement('a');
+    //var textToSave1 = textToSave;
+    hiddenElement.href = textToSave;
+    hiddenElement.target = '_blank';
+    hiddenElement.download = 'my_edited_html.html';
+    hiddenElement.click();
 }
 
 function onLoad() {
-    LeftDivHeight = document.getElementById('left').style.height
+    //LeftDivHeight = document.getElementById('left').style.height
+    //document.body.style.backgroundColor = document.location.search.split("?")[1]
 }
 
 /*function LeftDivMouseChange(LeftDiv) {
