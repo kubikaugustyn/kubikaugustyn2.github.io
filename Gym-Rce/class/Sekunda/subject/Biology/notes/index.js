f15e4c54ef3824e5ac8395bdce8a64cd640fadf1
raugustyn <Marika56gi>
2022-04-15 20:17:11 +0200

    Working update at 15. 4. 2022...

--- Gym-Rce/class/Sekunda/subject/Biology/notes/index.js
+++ Gym-Rce/class/Sekunda/subject/Biology/notes/index.js
@@ -2,6 +2,17 @@ var __author__ = "kubik.augustyn@post.cz"
 
 var notes = JSON.parse($$.Data.http("GET", "notes.json").responseText)
 
+function processNoteCommands(note) {
+    note = note.slice(1, note.length - 1)// <code> --> code
+    var arguments = note.split(" ")
+    for (var i = 0; i < arguments.length; i++) {
+        arguments[i] = arguments[i].replaceAll("<SPACE>", " ")
+    }
+    var func = arguments.shift()
+    console.log(func, arguments)
+    return func + ": " + arguments.join(" ")
+}
+
 function buildNotes(container, offset, data) {
     var noteIndex = 0
     for (var note of data) {
@@ -11,7 +22,7 @@ function buildNotes(container, offset, data) {
             if (typeof data[noteIndex + 1] === "object") {
                 html = "<underline>" + note + "</underline>"
             } else {
-                html = note
+                html = note[0] === "<" ? processNoteCommands(note) : note.replaceAll("\n", "")
             }
             container.innerHTML += $$.Data.encryption.string.multiply("<verticalline></verticalline>", offset) + (offset > 0 ? "-" : "") + html + "<br>"
         } else {
